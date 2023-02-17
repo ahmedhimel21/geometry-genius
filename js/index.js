ff1f96c74f083ccb6b3af854f9a3d384771d5c35
ahmedhimel21 <ahmedhimel000@gmail.com>
2023-02-18 01:42:02 +0600

    card two dom done

--- js/index.js
+++ js/index.js
@@ -8,4 +8,12 @@ document.getElementById('btn-card-1').addEventListener("click",function(){
   displayData(geometryType, geometryOperation);
 })
 
-
+// card two Dom manipulation
+document.getElementById('btn-card-2').addEventListener("click",function(){
+  serial+=1;
+  const input1Value = firstInputFieldValue('first-input2-field')
+  const input2Value = secondInputFieldValue('second-input2-field')
+  const geometryOperation = input1Value*input2Value;
+  const geometryType = document.getElementById('geometry2-type').innerText;
+  displayData(geometryType, geometryOperation);
+})
